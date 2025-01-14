7e1a763ef63f3820f599bee32f5ec0c962a4b426
Kailas <kailasninja@gmail.com>
2025-01-14 15:07:43 +0530

    Junit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class MainTest {
+
+    @BeforeAll
+    public void intit() {
+        System.out.println("Before all");
+    }
+
+    @BeforeEach
+    public void each() {
+        System.out.println("Before each");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("Test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Test2");
+    }
+}
