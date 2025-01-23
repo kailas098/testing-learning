6b67ffc71302fc0a98193fad9c66f1515a79e445
Kailas Nath S <kailasnath.s@zohocorp.com>
2025-01-23 15:41:51 +0530

    Xpath syntax
    Explored starts-with(), text(), contains() methods in xpath

--- src/test/java/SelenTest.java
+++ src/test/java/SelenTest.java
@@ -1,21 +1,49 @@
+
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Options;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class SelenTest {
     public static void main(String[] args) throws Exception {
-        FirefoxDriver driver = new FirefoxDriver();
+        FirefoxOptions options = new FirefoxOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        FirefoxDriver driver = new FirefoxDriver(options);
         driver.get("http://localhost:8080/Thoughts/");
 
-        //login 
-        driver.findElement(By.xpath("//*[@id=\"main-body\"]/form/input[1]")).sendKeys("kailas");
-        driver.findElement(By.xpath("//*[@id=\"main-body\"]/form/input[2]")).sendKeys("test123");
-        driver.findElement(By.xpath("//*[@id=\"main-body\"]/form/input[3]")).click();
+        loginTest(driver);
+        makeNewPostTest(driver);
+        deletePostTest(driver);
+        
+        driver.quit();
+    }
 
-        //go to profile
+    static void webDriverCon(WebDriver driver) {
+        driver.get("http://localhost:8080/Thoughts/");
+        Options options = driver.manage();
+        String source = driver.getPageSource();
+    }
+
+    static void clickNewUser(WebDriver driver) {
+        driver.findElement(By.xpath("//a[text()='New user?']"));
+    }
+
+    static void loginTest(WebDriver driver) {
+         driver.findElement(By.xpath("//input[starts-with(@name, 'username')]")).sendKeys("kailas");
+         driver.findElement(By.xpath("//input[starts-with(@name, 'password')]")).sendKeys("test123");
+         driver.findElement(By.xpath("//input[contains(@type, 'submit')]")).click();
+    }
+
+    static void makeNewPostTest(WebDriver driver) {
         driver.findElement(By.xpath("//*[@id=\"side-btns\"]/div[1]/a")).click();
-
-        // add sample post
         driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[3]/form/textarea")).sendKeys("Sample");
         driver.findElement(By.xpath("//*[@id=\"submit-btn\"]")).click();
+    }
+
+    static void deletePostTest(WebDriver driver) {
+        driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[1]/div[2]/a[1]")).click();
     }
 }
